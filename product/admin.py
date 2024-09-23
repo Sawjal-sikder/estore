ec8b08d0c513391cbdb4dce00980b0f51918a95a
Sawjal_sikder <mdsajalsikder@gmail.com>
2024-09-23 13:05:44 +0600

    Complete product database setup
    - Added Product, Category, and  models
    - Included fields for product details (name, description, pricing, SKU, stock, images, and status flags)
    - Created relationships between Category, SubCategory, and Product models
    - Implemented migration files for the database schema
    - Added slug fields for SEO-friendly URLs
    - Set up admin configurations for easy management of products and categories
    - Ready for CRUD operations and integration with the front-end

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'is_active', 'created_by', 'updated_by']
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ['-id', ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # This is a new object
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'is_active', 'created_by', 'updated_by']
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ['-id', ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # This is a new object
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'brand', 'sku', 'size', 'color', 'slug', 'is_active', 'created_by',
+                    'updated_by']
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ['-id', ]
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # This is a new object
+            obj.created_by = request.user
+        obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    thumbnail = models.ImageField(upload_to='category_thumbnails/', blank=True, null=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name='categories_created')
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name='categories_updated')
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    thumbnail = models.ImageField(upload_to='brand_thumbnails/', blank=True, null=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name='brands_created')
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name='brands_updated')
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    COLOR_CHOICES = [
+        ('red', 'Red'),
+        ('blue', 'Blue'),
+        ('green', 'Green'),
+        ('black', 'Black'),
+        ('white', 'White'),
+        ('yellow', 'Yellow'),
+    ]
+
+    # Define choices for size
+    SIZE_CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+    ]
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products')
+    thumbnail = models.ImageField(upload_to='product_thumbnails/', blank=True, null=True)
+    image1 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    sku = models.CharField(max_length=100, unique=True)
+    size = models.CharField(max_length=2, choices=SIZE_CHOICES, blank=True, null=True)  # Add size choices
+    color = models.CharField(max_length=10, choices=COLOR_CHOICES, blank=True, null=True)  # Add color choices
+    slug = models.SlugField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name='products_created')
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
+                                   related_name='products_updated')
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
