fe0ad3beefc443565ac2b2164be7267bf735972e
Md Sawjal Sikder <134833280+Sawjal-sikder@users.noreply.github.com>
2024-09-25 07:12:42 +0600

    product listing view

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', productList, name="productlist"),
+    path('search/', searchProduct, name="search"),
     path('details/', productDetails, name="productdetails"),
+    path('details/categoryProduct/<slug:slug>', categoryProduct, name="categoryProduct"),
+    path('details/brand/<slug:slug>', productByBrand, name="productByBrand"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,23 +1,55 @@
 from django.shortcuts import render
+from django.db.models import Count
 from .models import *
 
 
 # Create your views here.
-
-def productList(request):
-
-    all_product = Product.objects.filter(is_active=True)
+def getShareContext():
     short_product = Product.objects.filter(is_active=True).order_by('-created_date')[:3]
     categories = Category.objects.filter(is_active=True)
-    brands = Brand.objects.filter(is_active=True)
-
-    context = {
-        'products': all_product,
+    brands = Brand.objects.filter(is_active=True).annotate(product_count=Count('products'))
+    return {
         'categories': categories,
         'brands': brands,
         'short_product': short_product,
     }
 
+
+def productList(request):
+    all_product = Product.objects.filter(is_active=True)
+    context = {'products': all_product}
+    context.update(getShareContext())  # Merge shared context into the main context
+    return render(request, 'product/product-list.html', context)
+
+
+def categoryProduct(request, slug):
+    singleCategory = Category.objects.get(slug=slug)
+    all_product = Product.objects.filter(is_active=True, category=singleCategory)
+    context = {
+        'products': all_product,
+    }
+    context.update(getShareContext())
+    return render(request, 'product/product-list.html', context)
+
+
+def productByBrand(request, slug):
+    singleBrand = Brand.objects.get(slug=slug)
+    all_product = Product.objects.filter(is_active=True, brand=singleBrand)
+    context = {
+        'products': all_product,
+    }
+    context.update(getShareContext())
+    return render(request, 'product/product-list.html', context)
+
+
+def searchProduct(request):
+    all_product = Product.objects.none()
+    if request.method == "GET":
+        product = request.GET.get('product', '')
+        if product:
+            all_product = Product.objects.filter(is_active=True, name__icontains=product)
+    context = {'products': all_product, }
+    context.update(getShareContext())
     return render(request, 'product/product-list.html', context)
 
 
